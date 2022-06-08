c9647fb677fa5663662c676e9f7bc06c51850ee8
Konstantin <nemeshkv@gmail.com>
2022-06-08 23:22:39 +0500

    Initial commit

---
+++ src/VisaClassicCard.java
@@ -0,0 +1,22 @@
+class VisaClassicCard {
+    public String id;
+    public String cardHolder;
+    public int discount;
+    public int amount;
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    public void setCardHolder(String cardHolder){
+        this.cardHolder = cardHolder;
+    }
+    public int setDiscount(double setDiscount) {
+        if (discount > 0);
+        this.discount = discount;
+        return discount;
+    }
+    public void getDiscount(amount, discount) {
+        if (amount > 500);
+        discount = (int) (amount*discount);
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[]  args) {
+        VisaClassicCard card = new VisaClassicCard();
+        card.setId("1234567890");
+        card.setCardHolder("Konstantin");
+        card.setDiscount(0.5);
+    }
+        String getDiscount() {
+    }
+}
